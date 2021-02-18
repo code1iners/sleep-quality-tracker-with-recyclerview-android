94ef0e1c0c360151e781b6c570f354f09d9c54ee
Codeliner <codeliner96@gmail.com>
2021-02-19 02:28:51 +0900

    Updated header span in Grid recyclerview

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -63,6 +63,12 @@ class SleepTrackerFragment : Fragment() {
 
         // note. Add GridLayout
         val manager = GridLayoutManager(activity, 3)
+        manager.spanSizeLookup = object: GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int = when (position) {
+                0 -> 3
+                else -> 1
+            }
+        }
         binding.sleepList.layoutManager = manager
 
         // note. initialize item list adapter
